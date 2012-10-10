0a658e9df1399379395b2f5549bf9df33f2c869e
amarini <>
2012-10-10 22:22:32 +0000

    *** empty log message ***

--- test/SKIM_data_cfg.py
+++ test/SKIM_data_cfg.py
@@ -13,9 +13,9 @@ process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(-1))
 # ---- define the source ------------------------------------------------
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring([
-	'/store/data/Run2012C/DoubleMu/AOD/PromptReco-v1/000/197/770/7EF0FF77-77C3-E111-941A-5404A63886EE.root',
-	'/store/data/Run2012C/DoubleMu/AOD/PromptReco-v1/000/197/772/8E02C4D0-79C3-E111-9773-001D09F28EA3.root',
-	'/store/data/Run2012C/DoubleMu/AOD/PromptReco-v1/000/197/774/3C5FCCD6-79C3-E111-8249-001D09F25479.root'
+#	'/store/data/Run2012C/DoubleMu/AOD/PromptReco-v1/000/197/770/7EF0FF77-77C3-E111-941A-5404A63886EE.root',
+#	'/store/data/Run2012C/DoubleMu/AOD/PromptReco-v1/000/197/772/8E02C4D0-79C3-E111-9773-001D09F28EA3.root',
+#	'/store/data/Run2012C/DoubleMu/AOD/PromptReco-v1/000/197/774/3C5FCCD6-79C3-E111-8249-001D09F25479.root'
 ])
 )
 ## ---- define the output file -------------------------------------------
@@ -24,13 +24,13 @@ process.source = cms.Source("PoolSource",
 #    closeFileFast = cms.untracked.bool(True)
 #)
 # ---- filter the required HLT bits -------------------------------------
-process.hltFilter = cms.EDFilter('HLTHighLevel',
-    TriggerResultsTag  = cms.InputTag('TriggerResults','','HLT'),
-    HLTPaths           = cms.vstring('HLT_DoubleMu6_v*','HLT_DoubleMu7_v*','HLT_DoubleMu8_v*','HLT_Mu13_Mu8_v*'),
-    eventSetupPathsKey = cms.string(''),
-    andOr              = cms.bool(True), #----- True = OR, False = AND between the HLTPaths
-    throw              = cms.bool(False)
-)
+#process.hltFilter = cms.EDFilter('HLTHighLevel',
+#    TriggerResultsTag  = cms.InputTag('TriggerResults','','HLT'),
+#    HLTPaths           = cms.vstring('HLT_DoubleMu6_v*','HLT_DoubleMu7_v*','HLT_DoubleMu8_v*','HLT_Mu13_Mu8_v*'),
+#    eventSetupPathsKey = cms.string(''),
+#    andOr              = cms.bool(True), #----- True = OR, False = AND between the HLTPaths
+#    throw              = cms.bool(False)
+#)
 # ---- filter the required leptons -------------------------------------
 process.muonsFilter = cms.EDFilter('MuonRefSelector',
 	src	=	cms.InputTag("muons"),
@@ -65,13 +65,13 @@ process.Out = cms.OutputModule("PoolOutputModule",
 )
 
 # ---- first sequence: events that pass the trigger ---------------------
-process.s1 = cms.Sequence(process.hltFilter * process.muonsFilter * process.MuMuFilter)
+process.s1 = cms.Sequence(process.muonsFilter * process.MuMuFilter)
 process.p1 = cms.Path(process.s1)
 
-process.s2 = cms.Sequence(process.hltFilter * process.electronsFilter * process.ElElFilter)
+process.s2 = cms.Sequence(process.electronsFilter * process.ElElFilter)
 process.p2 = cms.Path(process.s2)
 
-process.s3 = cms.Sequence(process.hltFilter * process.muonsFilter * process.electronsFilter * process.ElMuFilter)
+process.s3 = cms.Sequence(process.muonsFilter * process.electronsFilter * process.ElMuFilter)
 process.p3 = cms.Path(process.s3)
 # ---- end path
 process.end = cms.EndPath(process.Out)
