2022bc689ec35b3cbb99441c7e40f2e3c704bbcd
amarini <>
2013-03-11 11:24:19 +0000

    bug fix

--- post-process/AddWeightsBatch.sh
+++ post-process/AddWeightsBatch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 FILENAME=$1
-ORIGIN="/eos/cms/store/user/webermat/zjgfiles_V00-07/"
-DEST="/eos/cms/store/user/amarini/zjets_V00-07/"
+ORIGIN="/eos/cms/store/user/webermat/zjgfiles_V00-07"
+DEST="/eos/cms/store/user/amarini/zjets_V00-07"
 EXEDIR=/afs/cern.ch/work/a/amarini/CMSSW_5_3_6/src/amarini/VPlusJets/post-process
 EOS='/afs/cern.ch/project/eos/installation/0.2.5/bin/eos.select'
 
@@ -23,15 +23,15 @@ cd \$WORKDIR
 
 FILE=$FILENAME
 
-[ ${MATCHPARTI} == 0 ] && { $EOS cp $ORIGIN/${FILENAME%%Part*}Part* ./ ; FILE=${FILENAME%%_Part*}.root; hadd \$FILE ${FILENAME%%Part*}Part*.root; }
+[ ${MATCHPARTI} == 0 ] && { $EOS cp -a $ORIGIN/${FILENAME%%Part*}Part* ./ ; FILE=${FILENAME%%_Part*}.root; hadd \$FILE ${FILENAME%%Part*}Part*.root; }
 [ ${MATCHPARTI} != 0 ] &&  [ ${MATCHPARTX} == 0 ] && exit 0;
 
-[ ${MATCHPARTI} != 0 ] && { ${EOS} cp $ORGIN/$FILENAME ./ ;}
+[ ${MATCHPARTI} != 0 ] && { ${EOS} cp -a ${ORIGIN}/${FILENAME} ./ ;}
 
 cd $EXECDIR
 ./PostProcess \$WORKDIR/\$FILE 0
 cd \$WORKDIR
-${EOS} cp $FILE $DEST
+${EOS} cp -a $FILE ${DEST}/
 
 EOF
 
